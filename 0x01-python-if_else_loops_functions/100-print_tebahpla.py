145d2072be2deaf834cc9333cf9ac29b7976f5eb
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-09-20 19:00:48 +0100

    Advanced Tasks

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for alpha in range(ord('z'), ord('a') - 1, -1):
+    if alpha % 2 == 0:
+        diff = 0
+    else:
+        diff = 32
+    print('{}'.format(chr(alpha - diff)), end='')
