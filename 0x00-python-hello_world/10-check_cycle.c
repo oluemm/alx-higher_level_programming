39091f0a900e7c79e0cceba389012daf69b610b0
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-09-20 10:00:48 +0100

    Linked list cycle: Technical Interview Question

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,2 +1,44 @@
 #include "lists.h"
-int check_cycle(listint_t *list);
+
+/**
+ * check_cycle - A Function in C that checks if a singly linked list
+ * has a cycle in it.
+ * @list: Linked list.
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr;
+	listint_t *nextptr;
+
+	ptr = list;
+	nextptr = list;
+
+	while (list != NULL && ptr != NULL && ptr->next != NULL)
+	{
+		list = list->next;
+		ptr = ptr->next->next;
+
+		if (list == ptr)
+		{
+			list = nextptr;
+			nextptr = ptr;
+			while (1)
+			{
+				ptr = nextptr;
+				while (ptr->next != list && ptr->next != nextptr)
+				{
+					ptr = ptr->next;
+				}
+				if (ptr->next == list)
+					break;
+
+				list = list->next;
+			}
+			return (1);
+		}
+	}
+
+	return (0);
+}
