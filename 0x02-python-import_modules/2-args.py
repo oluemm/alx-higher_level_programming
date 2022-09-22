485b3d97b016bdfc58cd9e71ead8dfaab49d2d06
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-09-22 10:32:21 +0100

    Basic Task:program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print(len(sys.argv)-1, " arguments: ")
+    count = len(sys.argv)-1
+    print(count, " arguments: ")
     for items in enumerate(sys.argv):
         if items[0] == 0:
             pass
