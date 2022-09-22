1524f1bbf8a2c58db1a3bd47857512bc73379837
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-09-22 15:09:27 +0100

    Basic Task:program that prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == '__main__':
     import sys
     count = len(sys.argv)-1
     if count == 0:
-        print(count, 'arguments: ')
+        print(f'{count} arguments.')
     elif count == 1:
-        print(count, 'argument: ')
+        print(f'{count} argument:')
     else:
-        print(count, 'arguments: ')
+        print(f'{count} arguments:')
 
     for i in range(count):
         print(f'{i+1}: {sys.argv[i+1]}')
