787414c94e4361c8a0d8aa4f31345820636b2544
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-09-25 13:56:00 +0100

    Basic Task : return multiple values

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        return length,None
+        return length, None
     else:
-        return length,sentence[0]
+        return length, sentence[0]
