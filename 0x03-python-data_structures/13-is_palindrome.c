9d3ea17f9a6ff537a34704868f4f66d2bd298224
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-09-26 09:48:51 +0100

    Basic Task : linked list palindrome checkerr

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -69,4 +69,3 @@ int is_palindrome(listint_t **head)
 
 	return (0);
 }
-
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -11,9 +12,8 @@
  */
 typedef struct listint_s
 {
-int n;
-
-struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -23,4 +23,3 @@ void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
-
