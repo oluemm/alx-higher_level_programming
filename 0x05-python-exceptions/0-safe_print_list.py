b576bd62d0bdabe73c1b82cbb1e8dc592ddbbf77
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-10-10 11:00:03 +0100

    basic task: function that prints x elements of a list.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}",end='')#print each element at index i
+            count += 1#keeping count of items in my_list
+        except IndexError:#catch errors relating to list indexing
+            break
+    print("")#to add a new line
+    return (count)
+    
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,3 @@
+# 0x05-python-exceptions
+- [0-safe_print_list.py](0-safe_print_list.py)
+- [1-safe_print_integer.py](1-safe_print_integer.py)
