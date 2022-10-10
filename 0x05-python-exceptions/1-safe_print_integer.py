1913ea7ac4b0d23c773f51fccfeb0ae0ac6eae4f
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-10-10 17:52:31 +0100

    basic task: a function that prints only an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))  # print given value
         return (True)  # return True if value is an integer
-    except ValueError:  # catch value error exception
+    except (TypeError, ValueError):  # catch value error exception
         return (False)  # return False
