193829b0a19fe9c35f022916dcd86c54f063c8b1
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-10-10 18:22:03 +0100

    adv task:  a function that prints an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        no_value_error = True
-    except ValueError as err:
-        no_value_error = False
-        print("Exception: ",err)
-    return no_value_error
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
