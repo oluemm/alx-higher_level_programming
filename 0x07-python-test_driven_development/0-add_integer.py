4c3684f7bb86df509e1584b2c6590e5dc95df430
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-10-16 17:30:35 +0100

    basic task: addition function with tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,10 +10,10 @@ def add_integer(a, b=98):
     """
     Returns the integer addition of a and b.
 
-    Float arguments are typecasted to ints before addition is performed.
+    `Float` arguments are typecasted to `ints` before addition is performed.
 
     Raises:
-        `TypeError`: If either of a or b is a non-integer and non-float.
+    `TypeError`: If either of a or b is a non-integer and non-float.
     """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
