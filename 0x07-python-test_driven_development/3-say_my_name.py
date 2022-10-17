b253bedca21aaae3add993d7702dc4a797c5d098
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-10-17 23:24:40 +0100

    basic task: function that prints to stdout

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Prints `My name is <first name> <last name>` to stdout."""
 
+
 def say_my_name(first_name, last_name=""):
     """Prints *My name is <first name> <last name>* to stdout.
 
