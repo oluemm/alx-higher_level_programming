5220fa4426c07a2e1b47625a14f2727100eec199
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-10-18 01:28:08 +0100

    basic task: rectangle class-> Public class attribute number_of_instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle:
     Attributes:
         number_of_instances (int): The number of rectangle instances created.
     """
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
