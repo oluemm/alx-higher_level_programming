b35d0fba77b41026e20b051fcbc4d2fcbfeff239
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-10-18 11:37:48 +0100

    advance task: implemented error checks

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Defines a matirx multiplier function"""
+
+
+def matrix_mul(m_a, m_b):
+# checks m_a & m_b  to determine if they're lists
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+
+# checks m_a  to determine if it is a list of lists
+    if not all (isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+
+# checks m_b to determine if it is a list of lists
+    if not all (isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+
+# checks empty list or list of lists
+    if m_a == [] or m_a == [[]]:
+        raise TypeError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise TypeError("m_b can't be empty")
+
+# checks if individual elements of matrix is an int or float
+    if not all((isinstance(elem, int) or isinstance(elem, float))
+        for elem in [num for row in m_a for num in row]):
+            raise TypeError("m_a should contain only integers or floats")
+    if not all((isinstance(elem, int) or isinstance(elem, float))
+        for elem in [num for row in m_b for num in row]):
+            raise TypeError("m_b should contain only integers or floats")
+
+# checks matirx row equality
+    if not all(len(row) == len(m_a[0]) for row in m_a):
+        raise TypeError("each row of m_a must should be of the same size")
+    if not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_b must should be of the same size")
+
+# checks matirx multiplication compatibility
+# for 2 matrix to be multiplied, len(m_a[0]) must be == len(m_b)
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
