d5a3e6faf8e4bb4d0f04302b38d584857ed8182b
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-10-27 18:24:25 +0100

    mandatory: Base class initialization

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,9 +8,9 @@ class Base():
     __nb_objects = 0  # private class attrib: id tracker
 
     def __init__(self, id=None):  # instantiate an object
-        if id != None:  # check if id is given
+        if id is not None:  # check if id is given
             self.id = id  # assign id
         else:
             #  type(self) is used to initialize the private class attrib
             type(self).__nb_objects += 1  # auto assign id if not given
-            self.id =  type(self).__nb_objects
+            self.id = type(self).__nb_objects
