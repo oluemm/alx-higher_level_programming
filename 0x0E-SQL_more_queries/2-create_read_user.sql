e161d1b69f2b73e47a46751898a13a6243825142
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-11-30 21:22:49 +0100

    create a user with SELECT privilege on a given dB

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- The user_0d_2 password should be set to user_0d_2_pwd
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
