88739aa57711bc38301e034b0db2d42f8f82358b
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-12-01 09:48:10 +0100

    left joins fix

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,7 +2,7 @@
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in AScending order by cities.id
 -- You can use only one SELECT statement
-SELECT c.id AS city_id,c.name AS city_name,s.name AS state_name 
+SELECT c.id,c.name,s.name
 FROM cities AS c left join states AS s 
 ON c.state_id=s.id
 ORDER BY c.id;
