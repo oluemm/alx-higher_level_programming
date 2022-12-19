d213491d5d8477156bd8c6aee4274b8e36d1230c
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-12-19 22:31:20 +0100

    fix(2-arguments.js): script that prints a message depending of the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-
-const { argv } = require('node:process');
-
-const argsLength = argv.length;
+const argsLength = process.argv.length;
 console.log(argsLength === 2 ? 'No argument' : argsLength === 3 ? 'Argument found' : 'Arguments found');
