ba8ab08e262d482b9d6e00f5ef570c79a502d4e0
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-12-20 08:45:57 +0100

    bug fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 /* Initialize an array of languages */
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-/* Loop through the array */
-for (const line of langs) {
-  /* print to stdout each element */
+// const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+// /* Loop through the array */
+// for (const line of langs) {
+//   /* print to stdout each element */
+//   console.log(line);
+// }
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
   console.log(line);
 }
