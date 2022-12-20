c96334b399fd130605b80037b5050319c4da074d
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-12-20 22:44:23 +0100

    bug fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print () {
+  print() {
     /* loop thru the height of the rectangle */
     for (let i = 0; i < this.height; i++) {
       /* print out 'X' repeating it the number of it's width times */
