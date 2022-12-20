16612fd28a5845f6c5e3cf59e72886b901180940
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-12-20 21:25:42 +0100

    feat(3-rectangle.js): print() that prints the rectangle using the character X

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+
+  print () {
+    /* loop thru the height of the rectangle */
+    for (let i = 0; i < this.height; i++) {
+      /* print out 'X' repeating it the number of it's width times */
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
