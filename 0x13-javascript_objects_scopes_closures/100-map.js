6fd867b10ba0372e35e66e42b3930a123cccdd77
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-12-21 16:53:45 +0100

    bug fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list
-console.log(list)
-const mapList = list.map((item, index) => item * index)
-console.log(mapList)
+const list = require('./100-data.js').list;
+console.log(list);
+const mapList = list.map((item, index) => item * index);
+console.log(mapList);
