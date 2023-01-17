82e9e9e0481a00d1f2432f58be3e748c1e497cfd
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-17 08:45:13 +0100

    fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script that takes in a URL and displays all HTTP methods the server will accept
-# curl -s -I $1 | awk '/^Allow/ {for( i=2; i<=NF;i++) printf " "$i; print""}'
+# script that takes in a URL and displays all HTTP methods the server will accept curl -s -I $1 | awk '/^Allow/ {for( i=2; i<=NF;i++) printf " "$i; print""}'
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
