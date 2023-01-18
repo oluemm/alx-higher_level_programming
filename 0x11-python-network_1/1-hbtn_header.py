01a138232c15d37109e3fc14eca84f1f20db9b63
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-18 09:13:26 +0100

    fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,10 +10,6 @@ import sys
 if __name__ == "__main__":
     URL = sys.argv[1]
     with urllib.request.urlopen(URL) as response:
-        # manually strolling thru the response to get
-        # filter out required header
-        X_Request_d = (response.__dict__.get('headers')
-                       .__dict__['_headers'][-3][1])
         # a more sustainable approach
         X_Request_d = dict(response.headers).get("X-Request-Id")
         print(f"{X_Request_d}")
