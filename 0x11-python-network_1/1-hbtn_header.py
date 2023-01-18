1d8ee6a1bf28435bcffe83c3982fb2995e3e81cb
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-18 09:21:34 +0100

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     URL = sys.argv[1]
     with urllib.request.urlopen(URL) as response:
         # a more sustainable approach
-        X_Request_d = dict(response.headers).get("X-Request-Id")
+        X_Request_d = response.headers.get("X-Request-Id")
         print(f"{X_Request_d}")
