03011b3cdc091e450130e84f3e0d658be1846d47
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-01-24 08:04:30 +0100

    shorten module docs

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays
-the value of the X-Request-Id variable found in the header of the response
+"""script that sends a request to the URL and displays the value of the 
+X-Request-Id
 """
 import urllib.request
 import sys
-
 
 if __name__ == "__main__":
     URL = sys.argv[1]
