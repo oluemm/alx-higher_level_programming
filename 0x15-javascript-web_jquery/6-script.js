0aa3b2526ab4f4d04a5e5ff9d91e318f8b36ecc7
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-02-20 19:29:55 +0100

    feat[contents]: replace text/contents in a tag

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,8 @@
+$('#update_header').click(function () {
+  $('header').html('New Header!!!'); // replaces the content
+});
+/* SECOND APPROACH
+$('#update_header').click(function () {
+  $('header').text('New Header!!!')
+})
+*/
